2a0d5f3835c8d1da841a574134259874fa54f651
Solofelang <solofelangseboko@gmail.com>
2022-06-18 15:46:17 +0200

    models+controller

---
+++ Models/StudentCourses.cs
@@ -0,0 +1,12 @@
+﻿namespace VGC.Models
+{
+    public class StudentCourses
+    {
+        [Key]
+        public int Id { get; set; }
+        public string StudentCourseName { get; set; }
+        public  string StudentCourseDesc { get; set; }
+        public string  LecturerName { get; set; }
+        public double Duration { get; set; }
+    }
+}
