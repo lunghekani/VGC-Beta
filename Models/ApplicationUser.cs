fefcd486c2246c77e87d2daa5bc85b0d8f1b0fa2
Shakeel Dollie <shakzz23@gmail.com>
2022-06-22 14:03:20 +0200

    ApplicationUser Validation

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace VGC.Models
 {
     public class ApplicationUser: IdentityUser
     {
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string Surname { get; set; }
-        public string  Address { get; set; }
+        public string?  Address { get; set; }
     }
 }
