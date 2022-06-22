94c308d5841db0441a41aaa8506c5dea2aac2dd2
Damian Banfield <Damian.banfield@gmail.com>
2022-06-22 11:29:38 +0200

    Added (add course content) MVC

---
+++ Models/AddCourseContent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace VGC.Models
+{
+	public class AddCourseContent
+	{
+		
+            [Key]
+
+		public int Id { get; set; }
+		public string CourseName { get; set; }
+		public string CourseDesc { get; set; }
+		public string LecturerName { get; set; }
+        public string CourseCode { get; set; }
+
+	}
+	
+}
+
---
+++ Views/AddCourseContent/AddCourseContent.cshtml.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace VGC.Views.AddCourseContent
+{
+    public class AddCourseContentModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
