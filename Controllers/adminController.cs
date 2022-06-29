7cb3228c62bde47bffb3bd1a4f8da20882e9df44
lunghekani <lunghivlanga@gmail.com>
2022-06-29 11:42:11 +0200

    Showing/Hiding UI According to user roles done

--- Controllers/adminController.cs
+++ Controllers/adminController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using VGC.Models;
 using VGC.ViewModels;
 
 namespace VGC.Controllers
 {
+    //[Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
         private readonly RoleManager<IdentityRole> _roleManager;
